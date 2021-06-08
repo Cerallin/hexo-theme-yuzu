42da2904a07681f30a90b0395030173c4c267ed6
Cerallin <cerallin@cerallin.top>
2021-06-08 13:48:30 +0800

    Add loading
    Signed-off-by: Cerallin <cerallin@cerallin.top>

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
   Theme.backToTop.register();
   Theme.showHeadBar.register();
+
+  $(".loading-wrapper").each((i, ele) => {
+    ele.style.display = 'none';
+  })
+
+  $(".page").each((i, ele) => {
+    $(ele).css("filter", 'none')
+  })
 });
