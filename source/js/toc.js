d1f5dfaf872c9d6e1608eae64beae663022a2726
Cerallin <cerallin@cerallin.top>
2022-04-17 20:40:09 +0800

    opt: 重构toc显示隐藏机制
    Signed-off-by: Cerallin <cerallin@cerallin.top>

--- source/js/toc.js
+++ source/js/toc.js
@@ -1,15 +1,14 @@
 const tocWrap = document.getElementById('post-toc');
 
 function toggleToc(button, expend, collapse) {
-  if (tocWrap.classList.contains('post-toc-show')) {
-    tocWrap.classList.remove('post-toc-show');
-    tocWrap.classList.add('post-toc-hide');
+  let expaned = tocWrap.getAttribute('data-expand') == "true";
+  tocWrap.setAttribute('data-expand', !expaned);
+
+  if (expaned) {
     button.innerText = collapse;
   } else {
-    tocWrap.classList.remove('post-toc-hide');
-    tocWrap.classList.add('post-toc-show');
     window.scroll({
-      top: tocWrap.offsetTop - 65, // plus header height
+      top: tocWrap.offsetTop - 65,
       behavior: "smooth"
     });
     button.innerText = expend;
