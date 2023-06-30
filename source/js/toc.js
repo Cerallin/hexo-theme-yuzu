d4a3c746817acf02cd9bd1867adfeccc6575874d
Cerallin <66366855+Cerallin@users.noreply.github.com>
2023-06-30 11:06:30 +0800

    feat: Make TOC aside (#7)

--- source/js/toc.js
+++ source/js/toc.js
@@ -1,16 +1,21 @@
-const tocWrap = document.getElementById('post-toc');
-
-function toggleToc(button, expend, collapse) {
-  let expaned = tocWrap.getAttribute('data-expand') == "true";
-  tocWrap.setAttribute('data-expand', !expaned);
-
-  if (expaned) {
-    button.innerText = collapse;
-  } else {
-    window.scroll({
-      top: tocWrap.offsetTop - 65,
-      behavior: "smooth"
+window.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
+      if (entry.intersectionRatio > 0) {
+        document.
+          querySelector(`.post-toc-item a[href="#${encodeURIComponent(id)}"]`).parentNode.
+          setAttribute('data-active', '');
+      } else {
+        document.
+          querySelector(`.post-toc-item a[href="#${encodeURIComponent(id)}"]`).parentNode.
+          removeAttribute('data-active');
+      }
     });
-    button.innerText = expend;
-  }
-}
+  });
+
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('article h3[id], article h4[id]').forEach((section) => {
+    observer.observe(section);
+  });
+});
