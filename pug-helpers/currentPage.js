4069356790d82287d3ddcb0b77b825f5d5561b55
Cerallin <cerallin@cerallin.top>
2023-10-14 15:34:17 +0800

    fix: tag page display error

--- pug-helpers/currentPage.js
+++ pug-helpers/currentPage.js
@@ -3,5 +3,6 @@ exports.default = function currentPage() {
   else if (this.is_post()) return "post";
   else if (this.is_category()) return "category";
   else if (this.is_archive()) return "archive";
+  else if (this.is_tag()) return "tag";
   return null;
 }
