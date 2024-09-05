1672f581f72f4efe0910be5098447bdefeac018e
Cerallin <cerallin@cerallin.top>
2024-09-05 07:48:01 +0000

    opt: Compat link-card to dark mode

--- scripts/link-card.js
+++ scripts/link-card.js
@@ -1,8 +1,10 @@
-// Copied and edited from https://github.com/OrangeX4/hexo-link-card/blob/main/index.js
+// Copied and modified from 
+// https://github.com/OrangeX4/hexo-link-card/blob/main/index.js
 //
 // MIT License
 //
 // Copyright (c) 2022 OrangeX4
+// Copyright (c) 2024 Cerallin
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -21,60 +23,19 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+//
 function linkCard(args) {
     const title = args.shift();
     const url = args.shift();
 
-    const backgroundColor = '#F6F6F6';
-    const titleColor = '#121212';
-    const descColor = '#999';
-
-    const styleText = [
-        'position: relative;',
-        'display: flex;',
-        'box-sizing: border-box;',
-        'flex-direction: row;',
-        'align-items: center;',
-        'width: 420px;',
-        'min-height: 84px;',
-        'border-radius: 8px;',
-        'max-width: 100%;',
-        'overflow: hidden;',
-        'margin: 16px auto;',
-        'padding: 12px 20px 9px 20px;',
-        `background-color: ${backgroundColor};`,
-    ].join('');
-
-    const innerStyleText = [
-        'display: flex;',
-        'overflow: hidden;',
-        'text-overflow: ellipsis;',
-        'max-height: 40px;',
-        'line-height: 1.25;',
-        `color: ${titleColor};`,
-    ].join('');
-
-    const svgStyleText = [
-        'display: flex;',
-        'font-size: 13px;',
-        'height: 18px;',
-        'line-height: 18px;',
-        `color: ${descColor};`,
-        'word-break: break-all;',
-        'text-overflow: ellipsis;',
-        'overflow: hidden;',
-    ].join('');
-
     return [
-        `<a target="_blank" href="${url}" style="${styleText}">`,
-        '  <span class="LinkCard-contents">',
-        `    <span style="${innerStyleText}">${title}</span>`,
-        `    <span style="${svgStyleText}">`,
-        '      <span style="display: inline-flex; align-items: center;">',
-        '        <i class="icon icon-link-45deg"></i>',
-        '      </span>',
-        `      <span>${url}</span>`,
+        `<a target="_blank" href="${url}" class="link-card">`,
+        `  <span class="link-card-title">${title}</span>`,
+        `  <span class="link-card-link">`,
+        '    <span class="link-card-icon">',
+        '      <i class="icon icon-link-45deg"></i>',
         '    </span>',
+        `    <span>${url}</span>`,
         '  </span>',
         '</a>',
     ].map(s => s.trim()).join('');
